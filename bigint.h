5b0ab960c712298cce31aee2020d2f95bbd1b1d7
Timm Knape <timm.knape@adamhall.com>
2020-10-16 11:35:36 +0200

    refactoring

--- bigint.h
+++ bigint.h
@@ -6,82 +6,99 @@
 namespace Big_Int {
 	class Int {
 		struct Rep {
-			unsigned short allocated;
-			unsigned short used;
+			using size_type = unsigned short;
+			size_type allocated;
+			size_type used;
 			int data[0];
 			int *begin() { return data; }
 			const int *begin() const { return data; }
-			int *used() { return data + used; }
-			const int *used() const { return data + used; }
+			int *cur() { return data + used; }
+			const int *cur() const { return data + used; }
 			int *end() { return data + allocated; }
 			const int end() const { return data + allocated; }
 		};
 		Rep *rep_ { nullptr };
-		static int empty_pattern(bool neg);
-		int empty_pattern() const;
-		void free();
+		using size_type = Rep::size_type;
+		void grow(unsigned capa);
+		void assure(unsigned capa);
 		void copy(const int *begin, const int *end);
-		void grow(int capa);
-		constexpr int block_size { 8 };
+		constexpr size_type block_size { 8 };
+		Int(const Rep *rep);
+		void free() { Int tmp { std::move(*this) }; }
 	public:
 		class Overflow {};
 		Int() {}
 		Int(int value);
 		Int(const Int &value);
 		Int(Int &&value);
-		~Int() { free(); }
+		Int &operator=(const Int &other);
+		Int &operator=(const Int &&other);
+		~Int();
 		bool negative() const;
-		int size() const;
+		size_type size() const;
 	};
-	static inline int Int::empty_pattern(bool neg) {
-		return neg ? ~0 : 0;
-	}
-	inline bool Int::negative() const {
-		return rep_ & rep_->used && rep_[rep_->used - 1] < 0;
-	}
-	inline int Int::empty_pattern() const {
-		return empty_pattern(negative());
-	}
-	inline unsigned short Int::size() const {
+	inline Int::size_type Int::size() const {
 		return rep_ ? rep_->used : 0;
 	}
-	inline Int::Int(int value) {
-		copy(&value, &value + 1);
+	inline bool Int::negative() const {
+		return size() > 0 && rep_->cur()[-1] < 0;
 	}
-	inline void Int::free() {
+	inline void Int::copy(const int *begin, const int *end) {
+		if (begin && end > begin) {
+			auto needed { end - begin };
+			assure(needed);
+			rep_->used = needed;
+			std::copy(begin, end, rep_->begin());
+			std::fill(rep_->cur(), rep_->end(), 0);
+		} else { free(); }
+	}
+	inline Int::Int(const int *begin, const int *end) {
+		copy(begin, end);
+	}
+	inline Int &Int::operator=(const Int &other) {
+		copy(begin, end);
+		return *this;
+	}
+	inline Int &Int::operator=(const Int &&other) {
+		std::swap(rep_, other.rep_);
+		return *this;
+	}
+	inline Int::Int(int value): Int { &value, &value + 1 } {}
+
+	inline Int::~Int() {
 		if (rep_) {
-			delete reinterpret_cast<char *>(rep_);
+			delete[] reinterpret_cast<char *>(rep_);
 			rep_ = nullptr;
 		}
 	}
-	inline void Int::grow(size_t capa) {
-		unsigned short size { rep_ ? rep_->allocated : 0 };
-		unsigned short used { rep_ ? rep_->used : 0 };
+
+	inline void Int::assure(unsigned capa) {
+		size_type size { rep_ ? rep_->allocated : 0 };
 		if (capa > size) {
 			capa = ((capa + block_size - 1) / block_size) * block_size;
 			if (capa > MAX_UNSIGNED_SHORT) { throw Overflow {}; }
+
 			Rep *tmp { new Rep (new char[sizeof(Rep) * capa * sizeof(char)]) {} };
-			tmp->allocated = capa;
 			tmp->used = 0;
+			tmp->allocated = capa;
 			free();
 			rep_ = tmp;
 		}
 	}
 
-	inline void Int::copy(const int *begin, const int *end) {
-		if (begin && end > begin) {
-			auto needed { end - begin };
-			grow(needed);
-			rep_->used = needed;
-			std::copy(begin, end, rep_->begin());
-			std::fill(rep_->used(), rep_->end(), empty_pattern());
-		} else { free(); }
+	inline void Int::grow(unsigned capa) {
+		size_type size { rep_ ? rep_->allocated : 0 };
+		if (capa > size) {
+			Int res {};
+			res.assure(capa);
+			res = *this;
+			*this = std::move(res);
+		}
 	}
+
 	inline Int::Int(Int &&value) {
-		std::swap(begin_, value.begin_);
-		std::swap(end_, value.end_);
+		*this = std::move(value);
 	}
-	inline Int::Int(const Int &value) {
-		copy(value.rep_->begin(), value.rep_->used());
-	}
+	inline Int::Int(const Rep *rep) : Int { rep ? rep->begin() : nullptr, rep ? rep->cur() : nullptr } {}
+	inline Int::Int(const Int &value): Int { value.rep_ } {}
 }
